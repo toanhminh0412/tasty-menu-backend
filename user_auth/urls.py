e81e1eeccbaf5c6e336ba2a58760b2090ea18eb1
Archie To <toanhminh0412@gmail.com>
2024-02-19 12:41:40 -0800

    Add an URL to serve static files

--- user_auth/urls.py
+++ user_auth/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import user_login, user_signup, user_logout
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('login', user_login, name='user_login'),
     path('logout', user_logout, name='user_logout'),
     path('signup', user_signup, name='user_signup'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
